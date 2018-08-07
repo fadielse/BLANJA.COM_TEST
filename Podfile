2ec49cd296bd74d46b031fa5fb6b56c159b796b4
fadil <fadil_boyz@ymail.com>
2018-08-07 20:35:32 +0700

    First Commit Real Weather

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Real Weather' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Real Weather
+
+  target 'Real WeatherTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Real WeatherUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ Real Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Real Weather
+//
+//  Created by FADIELSE on 07.08.18.
+//  Copyright © 2018 Fadielse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
